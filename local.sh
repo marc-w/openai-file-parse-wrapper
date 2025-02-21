fd386fc08fd09c9275bcbb3bdb4e93c5bcfabed3
Marc W <marc@marc-w.com>
2025-02-21 17:53:38 -0500

    Create local.sh

---
+++ local.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# caps matches Python OpenAI lib expectations
+export OPENAI_API_KEY="TKTKTKTKTK"
+export openai_project_name="Example PDF parse form marc-w.com"
+
+# Boolean, used if set (presence check)
+# checked as > 1, not on y/n/1/0
+export APPLICATION_DEBUG="y"
